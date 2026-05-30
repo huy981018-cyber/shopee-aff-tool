cd433c4ee1616c08f7981e4b9117d8ab259220a8
huy981018-cyber <huy981018@gmail.com>
2026-05-30 20:19:14 +0700

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-start "Python Server" cmd /k "cd /d %~dp0 && python relay.py"
+start "Python Server" cmd /k "cd /d %~dp0 && py relay.py"
 timeout /t 2 /nobreak >nul
 start "Ngrok" cmd /k "cd /d D:\Program Files\ngrok && ngrok http 8080 --domain=flavoring-dreamland-january.ngrok-free.dev"
